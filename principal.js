d62cf8a99f9cb8008d349414b176bb3949b76c61
millenona <162987404+millenona@users.noreply.github.com>
2024-09-25 09:05:00 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,13 @@
+var pacientes = document.querySelectorAll(".paciente")
+for (let i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent
+    var tdIMc = paciente.querySelector(".info-imc");
+    var imc = peso/(altura*altura);
+    paciente.querySelector(".info-imc").textContent = imc;
+    
+}
